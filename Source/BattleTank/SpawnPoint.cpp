78e3e110236483c717cf9354f18c53300e93c955
Lorenzo Iannaccaro <lorenzo.iannaccaro@gmail.com>
2020-04-28 15:26:47 +0200

    fix spawning chain relative to begin play

--- Source/BattleTank/SpawnPoint.cpp
+++ Source/BattleTank/SpawnPoint.cpp
@@ -1,6 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+#include "Kismet/GameplayStatics.h"
 #include "SpawnPoint.h"
 
 // Sets default values for this component's properties
@@ -19,9 +19,10 @@ void USpawnPoint::BeginPlay()
 {
 	Super::BeginPlay();
 
-	auto NewActor = GetWorld()->SpawnActor<AActor>(SpawnClass);
+	auto NewActor = GetWorld()->SpawnActorDeferred<AActor>(SpawnClass, GetComponentTransform());
 	if(!NewActor) {return;}
-	NewActor->AttachToComponent(this, FAttachmentTransformRules::KeepRelativeTransform);
+	NewActor->AttachToComponent(this, FAttachmentTransformRules::KeepWorldTransform);
+	UGameplayStatics::FinishSpawningActor(NewActor, GetComponentTransform());
 	
 }
 
