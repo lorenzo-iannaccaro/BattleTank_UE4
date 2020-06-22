89d3f819f11a9c9059b36598da5f7fad0f54cd06
Lorenzo Iannaccaro <lorenzo.iannaccaro@gmail.com>
2020-06-22 12:30:29 +0200

    remove unnecessary (and buggy for build the .exe)  header

--- Source/BattleTank/TankAIController.cpp
+++ Source/BattleTank/TankAIController.cpp
@@ -1,7 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Engine/World.h"
-#include "Viewport.h"
 #include "GameFramework/PlayerController.h"
 #include "Tank.h"
 #include "TankAimingComponent.h"
